41a9e891f4e7b2aab4c554b2bbde4d99552e8ad3
Alison Souza <alisonmsouza@yahoo.com.br>
2025-04-05 14:57:37 -0300

    change ami

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,30 +1,3 @@
-/*
-output "ami" {
-  value = aws_instance.ec2.ami
-}
-
-output "public_ip" {
-  value = aws_instance.ec2.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.ec2.id
-}
-
-output "subnet_id" {
-  value = aws_instance.ec2.subnet_id
-}
-
-output "key_name" {
-  value = aws_instance.ec2.key_name
-}
-
-output "vpc_id" {
-  value = aws_instance.ec2.vpc_security_group_ids
-}
-
-*/
-
 output "security_group_id" {
   value = aws_security_group.backend.id
 }
@@ -43,10 +16,6 @@ output "service_name" {
 
 output "task_definition" {
   value = aws_ecs_task_definition.my_task.arn
-}
-
-output "capacity_provider" {
-  value = aws_ecs_capacity_provider.my_capacity_provider.name
 }
 
 output "autoscaling_group" {
